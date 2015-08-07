ae1bca0f25aab994aecdcc99481e3292a5bb5fa9
john <jcostanzo01@manhattan.edu>
2015-08-06 22:20:05 -0400

    Javadoc and input val
    Added comments and completed input val

--- src/main/java/LabelMaker.java
+++ src/main/java/LabelMaker.java
@@ -5,6 +5,8 @@ import java.util.LinkedList;
 import java.util.Queue;
 import javax.swing.JOptionPane;
 
+/** Creates label text file. File is to be imported into an Excel spreadsheet and printed through 
+ * BarTender. */
 public class LabelMaker {
 	
 	private int aisleStart;
@@ -20,8 +22,12 @@ public class LabelMaker {
 	private boolean section2;
 	private boolean section4;
 	private boolean totemPole;
+	
+	// Queue for Exceptions. Invalid input Exceptions are added to this queue.
 	private Queue<Exception> errors = new LinkedList<Exception>();
 	
+	/** Initializes member fields with user's arguments. Calls error message if needed. Calls 
+	 * main. */
 	public LabelMaker(String aisleStart, String aisleEnd, String sectionStart, String sectionEnd, 
 			String levelStart, String levelEnd, String positionStart, String positionEnd, 
 			boolean aisle2, boolean aisle4, boolean section2, boolean section4, boolean totemPole) 
@@ -40,6 +46,7 @@ public class LabelMaker {
 		setSection4(section4);
 		setTotemPole(totemPole);
 		
+		// If there was invalid input
 		if (!errors.isEmpty()) {
 			errorMsg();
 		} else {
@@ -47,10 +54,15 @@ public class LabelMaker {
 		}
 	}
 	
-	//mutators
+	/** Mutator functions. Input validation where needed. If invalid input, added Exception to 
+	 * errors queue. */
   	public void setAisleStart(String aisleStart) {
   		try {
   	  		this.aisleStart = Integer.parseInt(aisleStart);
+  	  		if (this.aisleStart < 1) {
+  	  			Exception x = new Exception();
+  	  			errors.add(x);
+  	  		}
   		} catch (Exception x) {
   			errors.add(x);
   		}
@@ -59,7 +71,6 @@ public class LabelMaker {
   	public void setAisleEnd(String aisleEnd) {
   		try {
   			this.aisleEnd = Integer.parseInt(aisleEnd);
-  			
   			if (aisleStart > this.aisleEnd || this.aisleEnd > 30) {
   				Exception x = new Exception();
   				errors.add(x);
@@ -72,6 +83,10 @@ public class LabelMaker {
   	public void setSectionStart(String sectionStart) {
   		try {
   			this.sectionStart = Integer.parseInt(sectionStart);
+  			if (this.sectionStart < 1) {
+  				Exception x = new Exception();
+  	  			errors.add(x);
+  			}
 		} catch (Exception x) {
 			errors.add(x);
   		}
@@ -80,7 +95,6 @@ public class LabelMaker {
   	public void setSectionEnd(String sectionEnd) {
   		try {
   			this.sectionEnd = Integer.parseInt(sectionEnd);
-  			
   			if (sectionStart > this.sectionEnd || this.sectionEnd > 100) {
   				Exception x = new Exception();
   				errors.add(x);
@@ -93,6 +107,10 @@ public class LabelMaker {
   	public void setLevelStart(String levelStart) {
   		try {
   			this.levelStart = levelStart.charAt(0);
+  			if (this.levelStart < 65) {
+  				Exception x = new Exception();
+  	  			errors.add(x);
+  			}
   		} catch (Exception x) {
   			errors.add(x);
   		}
@@ -101,8 +119,7 @@ public class LabelMaker {
   	public void setLevelEnd(String levelEnd) {
   		try {
   			this.levelEnd = levelEnd.charAt(0);
-  			
-  			if (levelStart > this.levelEnd || levelStart < 65 || this.levelEnd > 90) {
+  			if (levelStart > this.levelEnd || this.levelEnd > 90) {
   				Exception x = new Exception();
   				errors.add(x);
   			}
@@ -114,6 +131,10 @@ public class LabelMaker {
   	public void setPositionStart(String positionStart) {
   		try {
   			this.positionStart = Integer.parseInt(positionStart);
+  			if (this.positionStart < 1) {
+  				Exception x = new Exception();
+  	  			errors.add(x);
+  			}
   		} catch (Exception x) {
   			errors.add(x);
   		}
@@ -122,7 +143,6 @@ public class LabelMaker {
   	public void setPositionEnd(String positionEnd) {
   		try {
   			this.positionEnd = Integer.parseInt(positionEnd);
-  			
   			if (positionStart > this.positionEnd) {
   				Exception x = new Exception();
   				errors.add(x);
@@ -133,48 +153,28 @@ public class LabelMaker {
   	}
   	
   	public void setAisle2(boolean aisle2) {
-  		try {
   			this.aisle2 = aisle2;
-  		} catch (Exception x) {
-  			errors.add(x);
-  		}
   	}
   	
   	public void setAisle4(boolean aisle4) {
-  		try {
   			this.aisle4 = aisle4;
-  		} catch (Exception x) {
-  			errors.add(x);
-  			
-  		}
   	}
   	
   	public void setSection2(boolean section2) {
-  		try {
   			this.section2 = section2;
-  		} catch (Exception x) {
-  			errors.add(x);
-  		}
   	}
 	
   	public void setSection4(boolean section4) {
-  		try {
   			this.section4 = section4;
-  		} catch (Exception x) {
-  			errors.add(x);
-  		}
   	}
   	
   	public void setTotemPole(boolean totemPole) {
-  		try {
   			this.totemPole = totemPole;
-  		} catch (Exception x) {
-  			errors.add(x);
-  		}
   	}
   	
+  	/** Creates error message pop up with instructions. */
   	public void errorMsg() {
-
+  		
         	String s1 = "Enter data fields in the following format:\n\n";
         	String s2 = "-Aisles are positive integers no greater than 30\n";
         	String s3 = "-Sections are positive integers no greater than 100\n";
@@ -184,11 +184,12 @@ public class LabelMaker {
         	String s7 = "\nPlease enter revised data and try again!\n";
   		
   		String msg = s1 + s2 + s3 + s4 + s5 + s6 + s7;
-
+  		
   		JOptionPane.showMessageDialog(null, msg, "Error",
                 JOptionPane.ERROR_MESSAGE);
   	}
   	
+  	/** Writes labels.txt file. Writes to desktop. */
 	public void main() {
 		
 		String desktop = System.getProperty("user.home") + "\\Desktop";
@@ -214,36 +215,44 @@ public class LabelMaker {
                             
                             output.print(s1 + "-" + s2 + "-" + s3 + "-" + s4);
                             
+                            /* Prints labels horizontally rather than vertically if Totem Poles
+                             is selected. */
                             if (totemPole) {
                             	output.print("\t");
                             } else {
-                            	output.print("\n");
+                            	output.println();
                             }
         				}
         			}
         			
+        			// Increments section by 2 or 4 if user specifies.
                     if (section2) {
                     	section++;
                     } else if (section4) {
                     	section +=3;
                     }
                     
+                    // Totem Pole option breaks horizontal print at section change.
                     if (totemPole) {
                     	output.println();
                     }
         		}
         		
+        		// Increments aisle by 2 or 4 if user specifies.
                 if (aisle2) {
                 	aisle++;
                 } else if (aisle4) {
                 	aisle += 3;
                 }
         	}
+        	// Prints error message if FileNotFoundException. Not sure how to handle otherwise.
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, e.toString(), "Error",
+	                JOptionPane.ERROR_MESSAGE);
 		}
-        JOptionPane.showMessageDialog(null, "Text file \"labels\" was successfully created on your desktop", "Success!",
+        // Prints Success! message and exits if file was written successfully.
+        JOptionPane.showMessageDialog(null, "Text file \"labels\" was successfully created on your "
+        		+ "desktop", "Success!",
                 JOptionPane.PLAIN_MESSAGE);
         System.exit(0);
 	}
--- src/test/java/LabelMakerTest.java
+++ src/test/java/LabelMakerTest.java
@@ -1,12 +1,12 @@
 import static org.junit.Assert.*;
-
+import java.io.File;
 import org.junit.Test;
 
 public class LabelMakerTest {
 
 	@Test
 	public void testMain() {
-		fail("Not yet implemented");
+		File standardLabels = new File("standardLabels.txt");
 	}
 
 }
